e60459234c8d940d78fe71564eeeeb266809e4d9
Solomon Sunday <ssdahymond@gmail.com>
2020-07-11 00:04:17 +0100

    updated 0Auth2.0 and secured it

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+
 passport.use(new GoogleStrategy());
 //app.get('/',(req,res) => {
 //res.send({Welcome: 'Solomon'});
