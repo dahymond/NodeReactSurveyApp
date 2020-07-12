9f6ac50f0227522ff7007612e92e8d0b3fc590a2
Solomon Sunday <ssdahymond@gmail.com>
2020-07-12 22:31:05 +0100

    completed the google authetication

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('.config/keys');
+const keys = require('./config/keys');
 
 
 
@@ -11,8 +12,11 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
+}, 
+(accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile:', profile);
    }
   )
 );
@@ -20,10 +24,14 @@ passport.use(new GoogleStrategy({
 //res.send({Welcome: 'Solomon'});
 //});
 
-app.get('/auth/google', passport.authenticate('google', {
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
     scope: ['profile', 'email']
   })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
