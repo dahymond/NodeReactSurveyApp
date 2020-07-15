b1ec9fc4fd4f87cbeaee56d85fb3384a27f3e64d
Solomon Sunday <ssdahymond@gmail.com>
2020-07-15 02:51:57 +0100

    started the cookie implementation on the application

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,30 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users'); 
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+
+});
+
 passport.use(
     new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, 
-(accessToken, refreshToken, profile, done) => {
-    new User({googleId: profile.id}).save()
+(accessToken, refreshToken, profile, done) => { 
+    User.findOne({ googleId: profile.id})
+      .then((existingUser) => {
+          if (existingUser){
+              //we already have a record with the given profile ID
+              done(null, existingUser);
+          } else {
+              //we don't have a user record with this ID, make a new record
+              new User({googleId: profile.id})
+               .save()
+               .then(user => done(null, user));
+          }
+      });
     // console.log('access token', accessToken);
     // console.log('refresh token', refreshToken);
     // console.log('profile:', profile);
